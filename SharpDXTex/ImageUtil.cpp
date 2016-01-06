b3893816052cfbdfba2012f06da7f8c24da7532a
oguna <nao.lk.118@gmail.com>
2016-01-06 10:24:43 +0900

    implemented loadFromXXXFromXXX

--- SharpDXTex/ImageUtil.cpp
+++ SharpDXTex/ImageUtil.cpp
@@ -71,6 +71,97 @@ SharpDXTex::ScratchImage^ SharpDXTex::ImageUtil::LoadFromWICMemory(array<byte>^ 
 SharpDXTex::ScratchImage^ SharpDXTex::ImageUtil::LoadFromWICMemory(array<byte>^ source, WICFlags flags) {
 	pin_ptr<byte> p = &source[0];
 	auto image = gcnew ScratchImage();
-	DirectX::LoadFromWICMemory(p, source->Length, (DWORD)flags, nullptr, *image->_native);
+	DirectX::LoadFromDDSMemory(p, source->Length, (DWORD)flags, nullptr, *image->_native);
+	return image;
+}
+
+
+SharpDXTex::ScratchImage^ SharpDXTex::ImageUtil::LoadFromDDSFile(System::String^ file, DDSFlags flags, [System::Runtime::InteropServices::Out] TexMetadata% metadata)
+{
+	auto nativeName = Marshal::StringToHGlobalUni(file);
+	auto image = gcnew SharpDXTex::ScratchImage();
+	auto nativeImage = image->_native;
+	DirectX::TexMetadata nativeMetadata;
+	auto hr = DirectX::LoadFromDDSFile((LPCTSTR)nativeName.ToPointer(), (DWORD)flags, &nativeMetadata, *nativeImage);
+	Marshal::FreeHGlobal(nativeName);
+	if (FAILED(hr)) {
+		throw(gcnew System::Exception());
+	}
+	metadata = SharpDXTex::TexMetadata(nativeMetadata);
+	return image;
+}
+
+SharpDXTex::ScratchImage^ SharpDXTex::ImageUtil::LoadFromDDSFile(System::String^ file, DDSFlags flags)
+{
+	auto nativeName = Marshal::StringToHGlobalUni(file);
+	auto image = gcnew SharpDXTex::ScratchImage();
+	auto nativeImage = image->_native;
+	auto hr = DirectX::LoadFromDDSFile((LPCTSTR)nativeName.ToPointer(), (DWORD)flags, nullptr, *nativeImage);
+	Marshal::FreeHGlobal(nativeName);
+	if (FAILED(hr)) {
+		throw(gcnew System::Exception());
+	}
+	return image;
+}
+
+SharpDXTex::ScratchImage^ SharpDXTex::ImageUtil::LoadFromDDSMemory(array<byte>^ source, DDSFlags flags, [System::Runtime::InteropServices::Out] TexMetadata% metadata) {
+	pin_ptr<byte> p = &source[0];
+	DirectX::TexMetadata nativeMetadata;
+	auto image = gcnew ScratchImage();
+	DirectX::LoadFromWICMemory(p, source->Length, (DWORD)flags, &nativeMetadata, *image->_native);
+	metadata = TexMetadata::TexMetadata(nativeMetadata);
+	return image;
+}
+
+SharpDXTex::ScratchImage^ SharpDXTex::ImageUtil::LoadFromDDSMemory(array<byte>^ source, DDSFlags flags) {
+	pin_ptr<byte> p = &source[0];
+	auto image = gcnew ScratchImage();
+	DirectX::LoadFromDDSMemory(p, source->Length, (DWORD)flags, nullptr, *image->_native);
+	return image;
+}
+
+
+
+SharpDXTex::ScratchImage^ SharpDXTex::ImageUtil::LoadFromTGAFile(System::String^ file, [System::Runtime::InteropServices::Out] TexMetadata% metadata)
+{
+	auto nativeName = Marshal::StringToHGlobalUni(file);
+	auto image = gcnew SharpDXTex::ScratchImage();
+	auto nativeImage = image->_native;
+	DirectX::TexMetadata nativeMetadata;
+	auto hr = DirectX::LoadFromTGAFile((LPCTSTR)nativeName.ToPointer(), &nativeMetadata, *nativeImage);
+	Marshal::FreeHGlobal(nativeName);
+	if (FAILED(hr)) {
+		throw(gcnew System::Exception());
+	}
+	metadata = SharpDXTex::TexMetadata(nativeMetadata);
+	return image;
+}
+
+SharpDXTex::ScratchImage^ SharpDXTex::ImageUtil::LoadFromTGAFile(System::String^ file)
+{
+	auto nativeName = Marshal::StringToHGlobalUni(file);
+	auto image = gcnew SharpDXTex::ScratchImage();
+	auto nativeImage = image->_native;
+	auto hr = DirectX::LoadFromTGAFile((LPCTSTR)nativeName.ToPointer(), nullptr, *nativeImage);
+	Marshal::FreeHGlobal(nativeName);
+	if (FAILED(hr)) {
+		throw(gcnew System::Exception());
+	}
+	return image;
+}
+
+SharpDXTex::ScratchImage^ SharpDXTex::ImageUtil::LoadFromTGAMemory(array<byte>^ source, [System::Runtime::InteropServices::Out] TexMetadata% metadata) {
+	pin_ptr<byte> p = &source[0];
+	DirectX::TexMetadata nativeMetadata;
+	auto image = gcnew ScratchImage();
+	DirectX::LoadFromTGAMemory(p, source->Length, &nativeMetadata, *image->_native);
+	metadata = TexMetadata::TexMetadata(nativeMetadata);
+	return image;
+}
+
+SharpDXTex::ScratchImage^ SharpDXTex::ImageUtil::LoadFromTGAMemory(array<byte>^ source) {
+	pin_ptr<byte> p = &source[0];
+	auto image = gcnew ScratchImage();
+	DirectX::LoadFromTGAMemory(p, source->Length, nullptr, *image->_native);
 	return image;
 }
--- SharpDXTex/ImageUtil.h
+++ SharpDXTex/ImageUtil.h
@@ -12,6 +12,16 @@ namespace SharpDXTex
 	private:
 		ImageUtil();
 	public:
+		static ScratchImage^ LoadFromDDSFile(System::String^ file, DDSFlags flags, [System::Runtime::InteropServices::Out] TexMetadata% metadata);
+		static ScratchImage^ LoadFromDDSFile(System::String^ file, DDSFlags flags);
+		static ScratchImage^ LoadFromDDSMemory(array<byte>^ source, DDSFlags flasgs, [System::Runtime::InteropServices::Out] TexMetadata% metadata);
+		static ScratchImage^ LoadFromDDSMemory(array<byte>^ source, DDSFlags flasgs);
+
+		static ScratchImage^ LoadFromTGAFile(System::String^ file, [System::Runtime::InteropServices::Out] TexMetadata% metadata);
+		static ScratchImage^ LoadFromTGAFile(System::String^ file);
+		static ScratchImage^ LoadFromTGAMemory(array<byte>^ source, [System::Runtime::InteropServices::Out] TexMetadata% metadata);
+		static ScratchImage^ LoadFromTGAMemory(array<byte>^ source);
+
 		static ScratchImage^ LoadFromWICFile(System::String^ file, WICFlags flags, [System::Runtime::InteropServices::Out] TexMetadata% metadata);
 		static ScratchImage^ LoadFromWICFile(System::String^ file, WICFlags flags);
 		static ScratchImage^ LoadFromWICMemory(array<byte>^ source, WICFlags flasgs, [System::Runtime::InteropServices::Out] TexMetadata% metadata);
